9f37765473c5beb8d4a4d7fbdb84bff7c8084267
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-12 11:28:04 -0500

    menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayMaze()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
