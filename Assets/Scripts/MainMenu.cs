f00698f4d54c576fe766ea5f119a04576d6e03de
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-12 19:23:49 -0500

    Play

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,6 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayMaze()
     {
-        Debug.Log("Test");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
